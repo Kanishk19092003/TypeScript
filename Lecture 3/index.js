8a96b8896d8bbdf613b1cc704451040ccf726cb1
Kanishk19092003 <kanishkyadav141@gmail.com>
2026-05-01 14:58:57 +0530

    function syntax

---
+++ Lecture 3/index.js
@@ -0,0 +1,11 @@
+"use strict";
+// write a program to add 2 numbers,
+//how to catch errors 
+// ts configuration file
+Object.defineProperty(exports, "__esModule", { value: true });
+const sum = (a, b) => {
+    return a + b;
+};
+console.log(sum(5, 4));
+//lalalalla
+//# sourceMappingURL=index.js.map
---
+++ Lecture 3/index.ts
@@ -0,0 +1,10 @@
+// write a program to add 2 numbers,
+//how to catch errors 
+// ts configuration file
+
+const sum = (a:number, b:number):number => {
+    return a+b
+}
+
+console.log(sum(5,4));
+//lalalalla
