20036a67041e27a6fc75f6c8a79792712ca7943c
Kanishk19092003 <kanishkyadav141@gmail.com>
2026-05-01 15:39:50 +0530

    type annotations

---
+++ Lecture 4/index.js
@@ -0,0 +1,15 @@
+"use strict";
+//type annotations
+Object.defineProperty(exports, "__esModule", { value: true });
+//number,string,any,void,undefined,null,binlint,boolean,symbol,object
+
+let myFavno = 10;
+let fullNAme = "kanishk";
+console.log(myFavno.toString);
+
+// let invalidResult:number =  myFavno + "years" // wrong type annotations or wrong input
+
+let computedValue = Math.sqrt(16);
+
+console.log(computedValue);
+//# sourceMappingURL=index.js.map
---
+++ Lecture 4/index.ts
@@ -0,0 +1,13 @@
+//type annotations 
+
+//number,string,any,void,undefined,null,binlint,boolean,symbol,object
+
+let myFavno:number = 10;
+let fullNAme:String = "kanishk";
+
+console.log(myFavno.toString);
+
+// let invalidResult:number =  myFavno + "years" // wrong type annotations or wrong input
+
+let computedValue:number = Math.sqrt(16);
+console.log(computedValue);
