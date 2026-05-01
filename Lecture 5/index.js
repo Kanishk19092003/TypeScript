66a9db95724b8b98b347c95947d9a0cb03ba4614
Kanishk19092003 <kanishkyadav141@gmail.com>
2026-05-01 15:57:02 +0530

    Boolean type annotations

---
+++ Lecture 5/index.js
@@ -0,0 +1,11 @@
+"use strict";
+// BOOLEAN AND BIGLNT
+Object.defineProperty(exports, "__esModule", { value: true });
+//syntax
+let isMyNameKanishk = true;
+// eg -
+function isEven(a) {
+    return a % 2 === 0;
+}
+console.log(isEven(5));
+//# sourceMappingURL=index.js.map
---
+++ Lecture 5/index.ts
@@ -0,0 +1,14 @@
+// BOOLEAN 
+
+//syntax
+let isMyNameKanishk:boolean = true;
+
+// eg -
+
+function isEven(a:number):boolean{
+    return a%2 === 0;
+}
+
+console.log(isEven(5));
+
+//BIGINT - for numbers larger than regular no
