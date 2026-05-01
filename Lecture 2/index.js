fd73e5d7ce12b80b48688caf1957de5618dce217
Kanishk19092003 <kanishkyadav141@gmail.com>
2026-05-01 14:40:37 +0530

    Basic syntax

---
+++ Lecture 2/index.js
@@ -0,0 +1,10 @@
+"use strict";
+// typescript  = js + some power
+
+console.log("hello");
+
+var num = 5; // right way of writing code
+
+// num = "vinod" // wrong ;
+
+num = 55; // right
---
+++ Lecture 2/index.ts
@@ -0,0 +1,8 @@
+// typescript  = js + some power
+
+console.log("hello");
+
+var num :number = 5; // right way of writing code
+
+// num = "vinod" // wrong ;
+num = 55; // right
