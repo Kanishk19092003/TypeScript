fbe5305e8645cdba00550839786de5985eff48c2
Kanishk19092003 <kanishkyadav141@gmail.com>
2026-05-02 17:02:57 +0530

    Arrays Intro

---
+++ Lecture 10/index.js
@@ -0,0 +1,11 @@
+"use strict";
+// Arrays in ts
+Object.defineProperty(exports, "__esModule", { value: true });
+//using square barackets
+const numbers = [1, 23, 4, 5, 5, 6];
+//using the constructor
+const num = new Array(1, 2, 3, 4, 5, 6);
+// Arrays of methods
+const names = Array.of("kanishk", "yadav", "avi");
+console.log(names[2]);
+//# sourceMappingURL=index.js.map
---
+++ Lecture 10/index.ts
@@ -0,0 +1,11 @@
+// Arrays in ts
+
+//using square barackets
+const numbers:number[] = [1,23,4,5,5,6];
+
+//using the constructor
+const num:number[] = new Array(1,2,3,4,5,6);
+
+// Arrays of methods
+const names:string[] = Array.of("kanishk","yadav","avi"); 
+console.log(names[2]);
