d0e0c565345263fd992c789feb68a533e293f4a7
Kanishk19092003 <kanishkyadav141@gmail.com>
2026-05-02 12:48:30 +0530

    update

--- Lecture 8/index.js
+++ Lecture 8/index.js
@@ -5,4 +5,10 @@ function greet(name, id) {
     console.log(`Welcome! ${name} and your id is ${id}`);
 }
 greet("kanishk", 1);
+//OR 
+const welcome = (name, id) => {
+    return `${name}`;
+};
+const welo = welcome("kanishk", 2);
+console.log(welo);
 //# sourceMappingURL=index.js.map
